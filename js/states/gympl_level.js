1f38e99159199c154804e00d1a4886f1758b1338
Marek Lukáš <tajnymag@gmail.com>
2016-03-27 12:48:05 +0200

    Přidání znovupoužitelného gamepadu
    - přidán custom.js se správně globálními funkcemi
    - lepší verze mobilního přesměrování (použití relativní cesty)
    - pár řádek předěláno kvůli Atom Beatify

--- js/states/gympl_level.js
+++ js/states/gympl_level.js
@@ -93,34 +93,7 @@ GumpleRush.Gympl.prototype = {
 
     if (!this.game.device.desktop) {
       this.game.input.onDown.add(this.celaObrazovkaMobil, this);
-
-      this.button_a = this.game.add.button(524, 307, "button_a", null, this, 1, 0, 1, 0);
-      this.button_a.fixedToCamera = true;
-      this.button_a.events.onInputOver.add(function() { stisk_a = true; });
-      this.button_a.events.onInputOut.add(function() { stisk_a = false; });
-      this.button_a.events.onInputDown.add(function() { stisk_a = true; });
-      this.button_a.events.onInputUp.add(function() { stisk_a = false; });
-
-      this.button_b = this.game.add.button(622, 307, "button_b", null, this, 1, 0, 1, 0);
-      this.button_b.fixedToCamera = true;
-      this.button_b.events.onInputOver.add(function() { stisk_b = true; });
-      this.button_b.events.onInputOut.add(function() { stisk_b = false; });
-      this.button_b.events.onInputDown.add(function() { stisk_b = true; });
-      this.button_b.events.onInputUp.add(function() { stisk_b = false; });
-
-      this.button_l = this.game.add.button(2, 307, "button_l", null, this, 1, 0, 1, 0);
-      this.button_l.fixedToCamera = true;
-      this.button_l.events.onInputOver.add(function() { stisk_l = true; });
-      this.button_l.events.onInputOut.add(function() { stisk_l = false; });
-      this.button_l.events.onInputDown.add(function() { stisk_l = true; });
-      this.button_l.events.onInputUp.add(function() { stisk_l = false; });
-
-      this.button_p = this.game.add.button(100, 307, "button_p", null, this, 1, 0, 1, 0);
-      this.button_p.fixedToCamera = true;
-      this.button_p.events.onInputOver.add(function() { stisk_p = true; });
-      this.button_p.events.onInputOut.add(function() { stisk_p = false; });
-      this.button_p.events.onInputDown.add(function() { stisk_p = true; });
-      this.button_p.events.onInputUp.add(function() { stisk_p = false; });
+      this.game.nacteniGamepadu();
     }
   },
   celaObrazovkaMobil: function() {
--- js/states/honsey_kong.js
+++ js/states/honsey_kong.js
@@ -47,7 +47,6 @@ GumpleRush.Honsey_Kong.prototype = {
     this.vytvoreniNovehoBarelu();
     this.game.time.events.loop(3000, this.vytvoreniNovehoBarelu, this);
 
-
     this.cursors = this.game.input.keyboard.createCursorKeys();
     this.wasd = {
       up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
@@ -68,36 +67,8 @@ GumpleRush.Honsey_Kong.prototype = {
 
     if (!this.game.device.desktop) {
       this.game.input.onDown.add(this.celaObrazovkaMobil, this);
-
-      this.button_a = this.game.add.button(524, 307, "button_a", null, this, 1, 0, 1, 0);
-      this.button_a.fixedToCamera = true;
-      this.button_a.events.onInputOver.add(function() { stisk_a = true; });
-      this.button_a.events.onInputOut.add(function() { stisk_a = false; });
-      this.button_a.events.onInputDown.add(function() { stisk_a = true; });
-      this.button_a.events.onInputUp.add(function() { stisk_a = false; });
-
-      this.button_b = this.game.add.button(622, 307, "button_b", null, this, 1, 0, 1, 0);
-      this.button_b.fixedToCamera = true;
-      this.button_b.events.onInputOver.add(function() { stisk_b = true; });
-      this.button_b.events.onInputOut.add(function() { stisk_b = false; });
-      this.button_b.events.onInputDown.add(function() { stisk_b = true; });
-      this.button_b.events.onInputUp.add(function() { stisk_b = false; });
-
-      this.button_l = this.game.add.button(2, 307, "button_l", null, this, 1, 0, 1, 0);
-      this.button_l.fixedToCamera = true;
-      this.button_l.events.onInputOver.add(function() { stisk_l = true; });
-      this.button_l.events.onInputOut.add(function() { stisk_l = false; });
-      this.button_l.events.onInputDown.add(function() { stisk_l = true; });
-      this.button_l.events.onInputUp.add(function() { stisk_l = false; });
-
-      this.button_p = this.game.add.button(100, 307, "button_p", null, this, 1, 0, 1, 0);
-      this.button_p.fixedToCamera = true;
-      this.button_p.events.onInputOver.add(function() { stisk_p = true; });
-      this.button_p.events.onInputOut.add(function() { stisk_p = false; });
-      this.button_p.events.onInputDown.add(function() { stisk_p = true; });
-      this.button_p.events.onInputUp.add(function() { stisk_p = false; });
+      this.game.nacteniGamepadu();
     }
-
 
     this.srdicka = this.game.add.image(this.game.world.width - 70, 10, "hearts");
     this.srdicka.cropEnabled = true;
@@ -109,7 +80,6 @@ GumpleRush.Honsey_Kong.prototype = {
     this.hudba.play();
 
     this.hit = this.game.add.audio("hit");
-
   },
   vytvoreniNovehoBarelu: function() {
     this.barel = this.barely.create(150, 20, "barel");
